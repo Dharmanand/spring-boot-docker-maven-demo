cb9d71fd7fd950a35e79743b8af770a7a4d66bdc
Dharmanand <Dharmanand@users.noreply.github.com>
2020-05-26 08:37:01 +0530

    uploaded the all files

---
+++ src/main/java/com/kd/demo/SpringBootDockerMavenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerMavenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerMavenDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kd/demo/SpringBootDockerMavenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kd.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerMavenDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
